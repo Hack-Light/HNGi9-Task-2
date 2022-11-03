e8f7a5ad4e95b624e19da5f6ac768966ea8b3476
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-11-03 12:32:59 +0100

    update

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.post("/", (req, res) => {
+  let { operation_type, x, y } = req.body;
+  let result;
+
+  //   operations object
+  let operation = {
+    adddition: x + y,
+    subtraction: x - y,
+    multiplication: x * y,
+    division: x / y,
+  };
+
+  //   additon: x + y,
+  if (
+    operation_type.includes("add") ||
+    operation_type.includes("sum") ||
+    operation_type.includes("plus")
+  ) {
+    result = operation.adddition;
+    operation_type = "addition";
+  }
+
+  //   subtraction: x - y,
+  if (
+    operation_type.includes("subtract") ||
+    operation_type.includes("minus") ||
+    operation_type.includes("less")
+  ) {
+    result = operation.subtraction;
+    operation_type = "subtraction";
+  }
+
+  //   multiplication: x * y,
+  if (operation_type.includes("multiply") || operation_type.includes("times")) {
+    result = operation.multiplication;
+    operation_type = "multiplication";
+  }
+
+  //   division: x / y,
+  if (operation_type.includes("divide") || operation_type.includes("by")) {
+    result = operation.division;
+    operation_type = "division";
+  }
+
+  res.json({
+    slackUsername: "Light",
+    operation_type: operation_type,
+    result: result,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`[stdout]: Server is up on port ${port}`);
+});
